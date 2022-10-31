604a37ff96d879d8fa84062b098d5e2baebec526
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-10-31 14:46:16 +0200

    Задание 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryHtml = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryHtml);
+let simpleGallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
