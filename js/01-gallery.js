fef36ea9ca90c682034cbc5209ab998b1493c7be
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-10-31 13:52:32 +0200

    Задание 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryHtml = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryHtml);
+gallery.addEventListener('click', onClick);
+let instance;
+function onClick(event) {
+  event.preventDefault();
+  const src = event.target.dataset.source;
+  const desc = event.target.alt;
+  instance = basicLightbox.create(`<img src="${src}" alt="${desc}">`, {
+    onShow: onShowModal,
+    onClose: onCloseModal,
+  });
+  instance.show();
+}
+function onShowModal() {
+  document.addEventListener('keydown', onEscape);
+}
+function onCloseModal() {
+  document.removeEventListener('keydown', onEscape);
+}
+function onEscape(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+  }
+}
